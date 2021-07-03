9c50d40536d77ad1078a3c541d34a17591eb79a1
Christopher Magee <frostydknight@gmail.com>
2021-07-03 16:38:48 -0500

    Update main.py
    Added Buttons and Title to the startup page.

--- main.py
+++ main.py
@@ -2,19 +2,44 @@ import tkinter as tk
 
 #Builds window
 window = tk.Tk()
+window.columnconfigure(0, weight=1, minsize=250)
+window.rowconfigure(0, weight=1, minsize=100)
+
 #Creates general layout
-title = tk.Label(text="Rock, Paper, Scissors!")
-title.pack()
-
-startGame = tk.Button(
-    text="Start Game",
-    width=25,
-    height=5,
-    bg="grey"
+title = tk.Label(
+    master=window,
+    text="Rock, Paper, Scissors!",
+    font=('Arial', 25),
+    padx=5,
+    pady=5
+).grid(
+    column=0, 
+    row=0, 
+    padx=5, 
+    pady=5
 )
-startGame.pack()
-
+startGame = tk.Button(
+    master=window,
+    text="Start Game",
+    width=15,
+    height=2,
+).grid(
+    row=1,
+    column=0,
+    padx=5,
+    pady=5
+)
+exitGame = tk.Button(
+    master=window,
+    text="Exit Game",
+    width=15,
+    height=2,
+).grid(
+    row=2,
+    column=0,
+    padx=5,
+    pady=5
+)
 
 #Launches the gui
 window.mainloop()
-
